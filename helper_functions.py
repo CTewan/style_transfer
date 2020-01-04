4c12a090eba4890ed764e5099b1c034402a99d8a
Tewan Chia <chia_tewan@hotmail.com>
2020-01-04 21:58:31 +0800

    Working model v1

---
+++ helper_functions.py
@@ -0,0 +1,294 @@
+import os
+
+import cv2
+import numpy as np
+
+from tensorflow.keras.applications import (
+        vgg19
+    )
+
+from tensorflow.keras.preprocessing.image import save_img, load_img
+
+##############################################################################
+#                             Get User input                                 #
+##############################################################################
+
+def get_content_and_style_image_path():
+    content_img_path = input(
+            "Enter path for content image "
+            "(Press 'Enter' to use default): "
+        )
+
+    content_img_path = content_img_path.strip()
+
+    style_img_path = input(
+            "Enter path for style image "
+            "(Press 'Enter' to use default): "
+        )
+
+    style_img_path = style_img_path.strip()
+
+    if content_img_path == "":
+        content_img_path = "Data/content_1.jpeg"
+
+    if style_img_path == "":
+        style_img_path = "Data/style_5.jpg"
+
+    while not os.path.isfile(content_img_path):
+        content_img_path = input(
+                "Please enter a valid path for content image "
+                "(Press 'Enter' to use default): "
+            )
+
+        content_img_path = content_img_path.strip()
+
+    while not os.path.isfile(style_img_path):
+        style_img_path = input(
+                "Enter path for style image "
+                "(Press 'Enter' to use default): "
+            )
+
+        style_img_path = style_img_path.strip()
+    return content_img_path, style_img_path
+
+def _check_valid_tuple(output_dim):
+    part_1 = output_dim[0] != "(" 
+    part_2 = output_dim[-1] != ")" 
+    part_3 = output_dim.count(",") != 1
+
+    return part_1 and part_2 and part_3
+
+def _check_dim_in_range(dim):
+    if dim > 255 or dim < 128:
+        return False
+
+    return True
+
+def _check_valid_dimensions(output_dim):
+    dimensions = output_dim[1:-1]
+    height = dimensions.split(",")[0].strip()
+    width = dimensions.split(",")[1].strip()
+
+    try:
+        height = int(height)
+        width = int(width)
+
+        if _check_dim_in_range(height) and _check_dim_in_range(width):
+            return True
+
+        return False
+
+    except:
+        return False
+
+def _convert_to_tuple(output_dim):
+    dimensions = output_dim[1:-1]
+    height = dimensions.split(",")[0].strip()
+    width = dimensions.split(",")[1].strip()
+
+    height = int(height)
+    width = int(width)
+
+    return (height, width)
+
+def get_output_dim():
+    output_dim = input(
+            "Enter the desired shape for output image. "
+            "(Press 'Enter' to use default). "
+            "Example: (128, 128)"
+        )
+    output_dim = output_dim.strip()
+
+    if output_dim == "":
+        output_dim = (265, 200)
+
+        return output_dim
+
+    invalid_dimensions = True
+    while invalid_dimensions:
+        valid_tuple = _check_valid_tuple(output_dim)
+
+        if valid_tuple:
+            valid_dimensions = _check_valid_dimensions(output_dim)
+
+            if valid_dimensions:
+                break
+
+        output_dim = input(
+                "Please enter a valid output shape. "
+                "(Press 'Enter' to use default). "
+                "Example: (128, 128): "
+            )
+        output_dim = output_dim.strip()
+
+    output_dim = convert_to_tuple(output_dim)
+    return output_dim
+
+def get_model_name():
+    available_models = ["VGG19"]
+
+    model_name = input(
+            "Enter desired model to use. "
+            "(Press 'Enter' to use default): "
+            "Available: {}: ".format(available_models)         
+        )
+    model_name = model_name.strip()
+
+    if model_name == "":
+        model_name == "VGG19"
+
+    while model_name not in available_models:
+        model_name = input(
+                "Please enter a valid model. "
+                "(Press 'Enter to use default): "
+                "Available: {}: ".format(available_models)
+            )
+
+        model_name = model_name.strip()
+
+    return model_name
+
+def get_style_weight():
+    style_weight = input(
+            "Enter style weight "
+            "(Press 'Enter' to use default). "
+            "Example: 0.25: "
+        )
+
+    style_weight = style_weight.strip()
+
+    if style_weight == "":
+        style_weight = 1.0
+
+    invalid_input = True
+    while invalid_input:
+        try:
+            style_weight = float(style_weight)
+            break
+
+        except:
+            style_weight = input(
+                    "Please enter a valid style weight "
+                    "(Press 'Enter' to use default). "
+                    "Example: 0.25: "
+                )
+
+            style_weight = style_weight.strip()
+
+    return style_weight
+
+def get_content_weight():
+    content_weight = input(
+            "Enter content weight "
+            "(Press 'Enter' to use default): "
+            "Example: 0.25"
+        )
+
+    content_weight = content_weight.strip()
+
+    if content_weight == "":
+        content_weight = 0.025
+
+    invalid_input = True
+    while invalid_input:
+        try:
+            content_weight = float(content_weight)
+            break
+
+        except:
+            content_weight = input(
+                    "Please enter a valid content weight "
+                    "(Press 'Enter' to use default): "
+                    "Example: 0.25"
+                )
+
+            content_weight = content_weight.strip()
+
+    return content_weight
+
+def get_variation_weight():
+    variation_weight = input(
+            "Enter variation weight "
+            "(Press 'Enter' to use default): "
+            "Example: 0.25"
+        )
+
+    variation_weight = variation_weight.strip()
+
+    if variation_weight == "":
+        variation_weight = 1.0
+
+    invalid_input = True
+    while invalid_input:
+        try:
+            variation_weight = float(variation_weight)
+            break
+
+        except:
+            variation_weight = input(
+                    "Enter variation weight "
+                    "(Press 'Enter' to use default): "
+                    "Example: 0.25"
+                )
+
+            variation_weight = variation_weight.strip()
+
+    return variation_weight
+##############################################################################
+#                              Preprocess Image                              #
+##############################################################################
+
+def _read_image(img_path):
+    # Reads image in BGR format
+    img = cv2.imread(img_path)
+
+    return img
+
+def _resize_image(img, shape=None):
+    if shape == None:
+        shape = (128, 128)
+    
+    img = cv2.resize(img, dsize=shape, interpolation=cv2.INTER_LINEAR)
+
+    return img
+
+def _expand_dimensions(img):
+    img = np.expand_dims(img, axis=0)
+
+    return img
+
+
+def _normalize_image(img, model_name="VGG19"):
+    if model_name == "VGG19":
+        img = vgg19.preprocess_input(img)
+
+    return img
+
+def preprocess_image(img_path, model_name="VGG19", shape=None):
+    img = _read_image(img_path=img_path)
+    img = _resize_image(img=img, shape=shape)
+    img = _expand_dimensions(img=img)
+    img = _normalize_image(img=img, model_name=model_name)
+
+    return img
+
+##############################################################################
+#                               Deprocess Image                              #
+##############################################################################
+def deprocess_image(img, mean_val):
+
+    img[:, :, 0] += mean_val[2]
+    img[:, :, 1] += mean_val[1]
+    img[:, :, 2] += mean_val[0]
+
+    img = img[:, :, ::-1]
+    img = np.clip(img, 0, 255).astype("uint8")
+
+    return img
+
+##############################################################################
+#                                  Save Image                                #
+##############################################################################
+
+def save_img(img_path, img):
+    cv2.imwrite(img_path, img)
---
+++ style_transfer_model.py
@@ -0,0 +1,353 @@
+
+import numpy as np
+
+from tensorflow.keras.applications import (
+        vgg19
+    )
+
+import tensorflow.keras.backend as K
+
+from tensorflow.keras.models import (
+        load_model
+    )
+
+from scipy.optimize import fmin_l_bfgs_b
+
+from helper_functions import (
+        get_content_and_style_image_path,
+        get_output_dim,
+        get_model_name,
+        get_style_weight,
+        get_content_weight,
+        get_variation_weight,
+        preprocess_image,
+        deprocess_image,
+        save_img
+    )
+
+##############################################################################
+#                             Model Related                                  #
+##############################################################################
+def _get_model(input_tensor, model_name="VGG19"):
+    if model_name == "VGG19":
+        model = vgg19.VGG19(
+                input_tensor=input_tensor,
+                include_top=False,
+                weights="imagenet"
+            )
+
+    """
+    elif model_name == "Xception":
+        model = Xception(
+                input_shape=input_shape,
+                include_top=False,
+                weights="imagenet"
+            )
+
+    elif model_name == "InceptionV3":
+        model = InceptionV3(
+                input_shape=input_shape,
+                include_top=False,
+                weights="imagenet"
+            )
+    """
+    return model
+
+def _get_layers(model):
+    layers = dict([(layer.name, layer.output) for layer in model.layers])
+
+    return layers
+
+def _get_placeholder_img(shape):
+    placeholder_img = K.placeholder(shape=shape, name="placeholder_image")
+
+    return placeholder_img
+
+def _get_model_input(content_img, style_img, output_img):
+    ipt = K.concatenate([content_img, style_img, output_img], axis=0)
+
+    return ipt
+
+def _get_imagenet_mean():
+    mean_val = [123.68, 116.779, 103.939]
+
+    return mean_val
+
+##############################################################################
+#                                   Loss                                     #
+##############################################################################
+
+def _content_loss(content_img, output_img):
+    content_loss = K.sum(K.square(output_img - content_img))
+
+    return content_loss 
+
+def _gram_matrix(x):
+    features = K.batch_flatten(K.permute_dimensions(x, (2, 0, 1)))
+    gram = K.dot(features, K.transpose(features))
+
+    return gram
+
+def _style_loss(style_img, output_img, height, width, channels):
+    size = height * width
+    channels = channels
+
+    input_style = _gram_matrix(style_img)
+    output_style = _gram_matrix(output_img)
+
+    style_loss_numerator = K.sum(K.square(input_style - output_style)) 
+    style_loss_denominator = 4 * (channels ** 2) * (size ** 2)
+    style_loss = style_loss_numerator / style_loss_denominator
+
+    return style_loss
+
+def _total_variation_loss(output_img, height, width):
+    a = K.square(
+            output_img[:, :height-1, :width-1, :] 
+            - output_img[:, 1:, :width-1, :]
+        )
+
+    b = K.square(
+            output_img[:, :height-1, :width-1, :]
+            - output_img[:, :height-1, 1:, :]
+        )
+
+    variation_loss = K.sum(K.pow(a + b, 1.25))
+
+    return variation_loss
+
+def _update_content_loss(loss, layers, content_weight):
+    print("In content loss")
+    layer = layers["block5_conv2"]
+    content_img = layer[0, :, :, :]
+    output_img = layer[2, :, :, :]
+    content_loss = _content_loss(
+            content_img=content_img,
+            output_img=output_img
+        )
+    loss = loss + content_weight * content_loss
+
+    return loss
+
+def _update_style_loss(loss, layers, style_weight, height, width, channels):
+    print("In style loss")
+    STYLE_LAYERS = ['block1_conv1', 'block2_conv1',
+                      'block3_conv1', 'block4_conv1',
+                      'block5_conv1']
+
+    for layer_name in STYLE_LAYERS:
+        layer_features = layers[layer_name]
+        style_img = layer_features[1, :, :, :]
+        output_img = layer_features[2, :, :, :]
+        style_loss = _style_loss(
+                style_img=style_img,
+                output_img=output_img,
+                height=height,
+                width=width,
+                channels=channels
+            )
+
+        loss = loss + (style_weight / len(STYLE_LAYERS)) * style_loss
+
+    return loss
+
+def _update_variation_loss(loss, output_img, variation_weight, height, width):
+    print("In variation loss")
+    variation_loss = _total_variation_loss(
+            output_img=output_img,
+            height=height,
+            width=width
+        )
+
+    loss = loss + variation_weight * variation_loss
+
+    return loss
+
+def _evaluate_loss_and_grad(x, function, height, width, channels):
+    x = x.reshape((1, height, width, channels))
+    loss_grad = function([x])
+    loss = loss_grad[0]
+
+    if len(loss_grad[1:]) == 1:
+        grad = loss_grad[1].flatten().astype("float64")
+
+    else:
+        grad = np.array(loss_grad[1:]).flatten().astype("float64")
+
+    return loss, grad
+
+class Evaluator(object):
+    def __init__(self, height, width, channels, output_img, outputs):
+        self.loss = None
+        self.grad = None
+        self.function = None
+        self.height = height
+        self.width = width
+        self.channels = channels
+        self.output_img = output_img
+        self.outputs = outputs
+
+
+    def get_loss(self, x):
+        loss, grad = _evaluate_loss_and_grad(
+                x=x,
+                function=self.function,
+                height=self.height,
+                width=self.width,
+                channels=self.channels
+            )
+
+        self.loss = loss
+        self.grad = grad
+
+        return self.loss
+
+    def get_grad(self, x):
+        grad = np.copy(self.grad)
+
+        self.loss = None
+        self.grad = None
+
+        return grad
+
+    def get_function(self):
+        # Given image, calculates loss and gradient
+        self.function = K.function([self.output_img], self.outputs)
+
+        #return self.function
+
+def _generate_image(x, evaluator, height, width, channels, mean_val):
+    ITERATIONS=20
+
+    #x = np.random.uniform(0, 255, (1, height, width, channels)) - 128
+    #x = K.placeholder(shape=(1, height, width, channels))
+
+    for i in range(ITERATIONS):
+        print("Start of iteration {}".format(i))
+        x, loss, info = fmin_l_bfgs_b(
+                evaluator.get_loss,
+                x.flatten(),
+                #x,
+                fprime=evaluator.get_grad,
+                maxfun=20)
+
+        print("Loss for iteration {}: {}".format(i, loss))
+
+        img = x.copy()
+        img = img.reshape(height, width, channels)
+        output = deprocess_image(img=img, mean_val=mean_val)
+        output_name = r"Output/style_img_iteration_{}.jpeg".format(i)
+
+        save_img(img=output, img_path=output_name)
+
+def main():
+    # Get user inputs - image path, output shape, model
+    content_img_path, style_img_path = get_content_and_style_image_path()
+    output_dim = get_output_dim() # Default (128, 128)
+    model_name = get_model_name()
+    style_weight = get_style_weight()
+    content_weight = get_content_weight()
+    variation_weight = get_variation_weight()
+
+    # Preprocess images (1, 128, 128, 3)
+    content_img = preprocess_image(
+            img_path=content_img_path,
+            model_name=model_name,
+            shape=output_dim
+        )
+    style_img = preprocess_image(
+            img_path=style_img_path,
+            model_name=model_name,
+            shape=output_dim
+        )
+
+    dummy_img = preprocess_image(
+            img_path=content_img_path,
+            model_name=model_name,
+            shape=output_dim
+        )
+
+    content_img = K.variable(content_img)
+    style_img = K.variable(style_img)
+
+    content_shape = content_img.get_shape().as_list()
+    height = content_shape[1]
+    width = content_shape[2]
+    channels = content_shape[3]
+    input_shape = (height, width, channels)
+    output_shape = content_img.shape
+    output_img = _get_placeholder_img(shape=output_shape)
+    
+    # Prepare model
+    imagenet_mean = _get_imagenet_mean()
+
+    ipt = _get_model_input(
+            content_img=content_img,
+            style_img=style_img,
+            output_img=output_img
+        )
+    model = _get_model(input_tensor=ipt, model_name=model_name)
+    
+    # Calculate loss
+    layers = _get_layers(model=model)
+
+    loss = K.variable(0.0)
+    loss = _update_content_loss(
+            loss=loss,
+            layers=layers,
+            content_weight=content_weight
+        )
+    loss = _update_style_loss(
+            loss=loss,
+            layers=layers,
+            style_weight=style_weight,
+            height=height,
+            width=width,
+            channels=channels
+        )
+    loss = _update_variation_loss(
+            loss=loss,
+            output_img=output_img,
+            variation_weight=variation_weight,
+            height=height,
+            width=width
+        )
+
+    grads = K.gradients(loss, output_img)
+    outputs = [loss]
+
+    if isinstance(grads, (list, tuple)):
+        outputs += grads
+    else:
+        outputs.append(grads)
+
+    evaluator = Evaluator(
+            height=height,
+            width=width,
+            channels=channels,
+            output_img=output_img,
+            outputs=outputs
+        )
+
+    evaluator.get_function()
+
+    _generate_image(
+            x=dummy_img,
+            evaluator=evaluator,
+            height=height,
+            width=width,
+            channels=channels,
+            mean_val=imagenet_mean
+        )
+
+if __name__ == "__main__":
+    K.clear_session()
+    main()
+
+    
+
+    
+
+
+
+     
